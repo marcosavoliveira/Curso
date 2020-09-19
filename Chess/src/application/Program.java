ea2d8b4004f9587dc58302be82cc2809611bed1b
marcosavoliveira <marcosvieira1988@gmail.com>
2020-09-18 23:05:21 -0300

    Correção Main

--- Chess/src/application/Program.java
+++ Chess/src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import boardgame.Position;
 
 public class Program {
-}
 
-public static void main(String[] args) {
-	Position pos = new Position(3, 5);
-	pos.toString();
+	public static void main(String[] args) {
+		Position pos = new Position(3, 5);
+		pos.toString();
+	}
 }
