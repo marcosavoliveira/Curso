5c060cc2a00a544c2e0e5db4472fcee086659260
marcosavoliveira <marcosvieira1988@gmail.com>
2020-09-19 00:32:48 -0300

    Colocando as primeiras peças

--- Chess/src/boardgame/Board.java
+++ Chess/src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.ChessPiece;
+
 public class Board {
 	private int rows;
 	private int columns;
@@ -33,5 +35,9 @@ public class Board {
 
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+	public void placePiece(Piece piece,Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 }
